7e7e8a9781142ce79a9ea71c541830b079a783b7
Amol Ghotale <amol9.ag@gmail.com>
2022-01-17 09:55:54 +0530

    ADDED and MODIFIED: equalsTo function

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -4,23 +4,45 @@ import java.util.Scanner;
 
 public class LineComparision {
 
+   static double length1;
+    static double length2;
+    static double x1;
+    static double x2;
+    static double y1;
+    static double y2;
     static public void computeTo() {
         System.out.println("Welcome to line comparision Program");
-        
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of x1= ");
-        double x1 = sc.nextDouble();
+        x1 = sc.nextDouble();
         System.out.println("Enter the value of x1= ");
-        double x2 = sc.nextDouble();
+        x2 = sc.nextDouble();
         System.out.println("Enter the value of x2= ");
-        double y1 = sc.nextDouble();
+        y1 = sc.nextDouble();
         System.out.println("Enter the value of y1= ");
-        double y2 = sc.nextDouble();
+        y2 = sc.nextDouble();
         System.out.println("Enter the value of y2= ");
-        double length = (Math.sqrt(Math.pow((x2-x1),2))+Math.pow((y2-y1) ,2));
+        length1 = (Math.sqrt(Math.pow((x2-x1),2))+Math.pow((y2-y1) ,2));
+        length2 = (Math.sqrt(Math.pow((x2-x1),2))+Math.pow((y2-y1) ,2));
+    }
+
+
+    static public void equalsTo() {
+        if (length1==length2) {
+            System.out.println("Both lines are equal");
+
+        }  else if (length1>length2) {
+            System.out.println("Line 1 is greater than Line 2");
+
+        } else {
+            System.out.println("Line 2 is greater than Line 1");
+        }
 
     }
-    public static void main(String[] args) {
 
+
+    public static void main(String[] args) {
+        double lenght1, lenth2;
     }
 }
