7b63f9fa77a315929594554d881cf1582fed19ff
Amol Ghotale <amol9.ag@gmail.com>
2022-01-17 10:06:43 +0530

    ADDED,MODIFIED: variables

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -10,21 +10,35 @@ public class LineComparision {
     static double x2;
     static double y1;
     static double y2;
+    static double x3;
+    static double x4;
+    static double y3;
+    static double y4;
     static public void computeTo() {
         System.out.println("Welcome to line comparision Program");
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of x1= ");
         x1 = sc.nextDouble();
-        System.out.println("Enter the value of x1= ");
-        x2 = sc.nextDouble();
         System.out.println("Enter the value of x2= ");
-        y1 = sc.nextDouble();
+        x2 = sc.nextDouble();
         System.out.println("Enter the value of y1= ");
-        y2 = sc.nextDouble();
+        y1 = sc.nextDouble();
         System.out.println("Enter the value of y2= ");
+        y2 = sc.nextDouble();
+
+        System.out.println("Enter the value of x3= ");
+        x3 = sc.nextDouble();
+        System.out.println("Enter the value of x4= ");
+        x4 = sc.nextDouble();
+        System.out.println("Enter the value of y3= ");
+        y3 = sc.nextDouble();
+        System.out.println("Enter the value of y4= ");
+        y4 = sc.nextDouble();
         length1 = (Math.sqrt(Math.pow((x2-x1),2))+Math.pow((y2-y1) ,2));
-        length2 = (Math.sqrt(Math.pow((x2-x1),2))+Math.pow((y2-y1) ,2));
+        System.out.println("length if Line1 is= " +length1);
+        length2 = (Math.sqrt(Math.pow((x4-x3),2))+Math.pow((y4-y3) ,2));
+        System.out.println("length if Line2 is= " +length2);
     }
 
 
@@ -43,6 +57,8 @@ public class LineComparision {
 
 
     public static void main(String[] args) {
-        double lenght1, lenth2;
+        computeTo();
+        equalsTo();
+        System.out.println("Try with others values");
     }
 }
